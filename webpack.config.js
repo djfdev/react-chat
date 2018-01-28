ef0b0fa58dbe12e8c642327ffc37851e850feec7
Daniel Fried <djfdev@gmail.com>
2018-01-28 14:38:07 -0800

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' }
+      }
+    ]
+  },
+  devtool: process.env.NODE_ENV === 'production'
+    ? 'source-map'
+    : 'eval-source-map',
+  plugins: [HtmlWebpackPluginConfig]
+}
