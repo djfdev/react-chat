6f7aa80ef34b3b404d1cbe713c8858dc9baacadb
Daniel Fried <djfdev@gmail.com>
2018-01-28 18:40:39 -0800

    Add responsive scrolling to MessageList component

--- src/components/MessageList/index.js
+++ src/components/MessageList/index.js
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import './index.css'
 
 import Message from '../Message'
 
-const MessageList = props =>
-  <ul className='message-list'>
-    {props.messages.map(message => {
-      return (
-        <li className='message-list__item' key={`message-${message.id}`}>
-          <Message {...message} />
-        </li>
-      )
-    })}
-  </ul>
+class MessageList extends Component {
+  constructor (props) {
+    super(props)
+
+    this.scrollToBottom = this.scrollToBottom.bind(this)
+  }
+
+  componentDidMount () {
+    window.addEventListener('resize', this.scrollToBottom)
+  }
+
+  componentDidUpdate () {
+    this.scrollToBottom()
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener(this.scrollToBottom)
+  }
+
+  scrollToBottom () {
+    this.list.scrollIntoView(false)
+  }
+
+  render () {
+    return (
+      <ul ref={list => { this.list = list }} className='message-list'>
+        {this.props.messages.map(message => {
+          return (
+            <li className='message-list__item' key={`message-${message.id}`}>
+              <Message {...message} />
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+}
 
 MessageList.propTypes = {
   messages: PropTypes.array
